f16ea6ec833337ddc56656d318803e3b167ef5e6
timothyryeyn <timothy.perez@cambridge.org>
2025-08-29 14:43:01 +0800

    dynamic loading with added fallback page

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 export default {
 	async fetch(request, env) {
-		const assetReq = new Request(
-			new URL('/schoolsupporthub.cambridge.org.html', request.url)
-		)
-		const resp = await env.ASSETS.fetch(assetReq)
+		const url = new URL(request.url)
+		const host = (url.hostname || '').toLowerCase()
+
+		const assetReq = new Request(new URL(`/${host}.html`, request.url))
+		let resp = await env.ASSETS.fetch(assetReq)
+
+		if (resp.status === 404) {
+			resp = await env.ASSETS.fetch(
+				new Request(new URL(`/cambridge.org.html`, request.url))
+			)
+		}
 
 		return new Response(resp.body, {
 			status: 503,
