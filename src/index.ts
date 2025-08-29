ae673dd7ece2df73e68fa8af88a6d0bfaead1cfd
timothyryeyn <timothy.perez@cambridge.org>
2025-08-29 12:53:58 +0800

    Load page from assets

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 export default {
-	async fetch(request: Request): Promise<Response> {
-		/**
-		 * Replace `remote` with the host you wish to send requests to
-		 */
-		const remote = "https://example.com";
+	async fetch(request, env) {
+		const assetReq = new Request(
+			new URL('/schoolsupporthub.cambridge.org.html', request.url)
+		)
+		const resp = await env.ASSETS.fetch(assetReq)
 
-		return await fetch(remote, request);
-	},
-};
+		return new Response(resp.body, {
+			status: 503,
+			headers: {
+				'Content-Type': 'text/html; charset=UTF-8',
+				'Cache-Control': 'no-store',
+				'Retry-After': '3600'
+			}
+		})
+	}
+}
